227f16462e7c84f9b8dc2d05c355ad0214671938
kmerten10 <kelsey.merten@gmail.com>
2023-10-13 23:11:19 -0500

    Filtered events by city

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents, extractLocations } from './api';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
+
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  //why does useEffect have to go first?
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ? allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
 
   return (
     <div className="App">
       <NumberOfEvents />
-      <CitySearch />
-      <EventList />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <EventList events={events} />
     </div>
   );
 }
